8d101053e299b106b19bc3958fe25ce62ff445dc
root <root@ming-4.maas>
2021-05-12 05:55:38 +0000

    feature:data-clean

--- data-preprocess/data-clean/handler.py
+++ data-preprocess/data-clean/handler.py
@@ -2,6 +2,7 @@ from minio import Minio
 import pandas as pd
 import numpy as np
 from datetime import datetime, timedelta
+import pika
 def handle(req):
 
     client = Minio(
@@ -11,6 +12,14 @@ def handle(req):
         secure = False
     )
 
+    credentials = pika.PlainCredentials('user', 'user')
+
+    connection = pika.BlockingConnection(pika.ConnectionParameters('10.20.1.54', '30672', '/', credentials))
+
+    channel = connection.channel()
+
+    channel.queue_declare(queue='hello')
+
     try:
         client.fget_object('time-parse', 'time-parse.csv', '/home/app/time-parse.csv')
     except ResponseError as err:
@@ -18,14 +27,24 @@ def handle(req):
 
     data = pd.read_csv("/home/app/time-parse.csv").copy()
 
-    # normalization
     data = data.round(2)
 
-    # condition list 存下來
-    data, condition = anomalyDetection(data=data)
+    # 每個都要加
+    for i in range(data.shape[0]):
+        data["LocalTime"][i] = datetime.strptime(data["LocalTime"][i], '%Y-%m-%d %H:%M:%S')
+    data = data.set_index('LocalTime') 
+    # 
+
+    data, condition = anomalyDetection(data)
+
+
+    new_condition = ','.join(str(e) for e in condition.copy())
+    channel.basic_publish(exchange='', routing_key='hello', body=new_condition)
+    connection.close()
 
     data.to_csv("/home/app/data-clean.csv")
 
+    found = client.bucket_exists("data-clean")
     if not found:
         client.make_bucket("data-clean")
     else:
@@ -39,8 +58,10 @@ def handle(req):
     return 1
 
 
-def anomalyDetection(method = 'chebyshev', value_maximun = 40, value_minimun = 0, data):
-
+def anomalyDetection(data, method = 'chebyshev', value_maximun = 40, value_minimun = 0):
+    # 全域變數
+    target_field = 'Temp'
+    
     def timeParser(date, time, day = 0):
         _date = (date + timedelta(days = day)).strftime('%Y-%m-%d')
         return datetime.strptime(_date + ' ' + time, '%Y-%m-%d %H:%M:%S')
