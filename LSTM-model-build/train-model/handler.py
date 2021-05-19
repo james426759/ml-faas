9925cfcd0366390c62e9f4ec5324e847937fe0de
root <root@ming-4.maas>
2021-05-19 04:03:21 +0000

    LSTM Model

--- LSTM-model-build/train-model/handler.py
+++ LSTM-model-build/train-model/handler.py
@@ -5,26 +5,81 @@ from keras.layers.normalization import BatchNormalization
 from keras.optimizers import Adam
 from keras.callbacks import EarlyStopping, ModelCheckpoint
 import tensorflow as tf
+from minio import Minio
+from minio.error import S3Error
+import json
 
 def handle(req):
-    拉modle參數檔, x_train, y_train 下來
 
-    model = modelLoad(modle參數檔)
+    client = Minio(
+        "10.20.1.54:30020",
+        access_key="admin",
+        secret_key="secretsecret",
+        secure = False
+    )
+
+    try:
+        client.fget_object('train-model-setup', 'model_setup.h5', '/home/app/model_setup.h5')
+        client.fget_object('train-dataset', 'xt.json', '/home/app/xt.json')
+        client.fget_object('train-dataset', 'yt.json', '/home/app/yt.json')
+    except ResponseError as err:
+        print(err)
+
+    x_dict = ""
+    with open('/home/app/xt.json', 'r') as obj:
+        x_dict = json.load(obj)
+
+    y_dict = ""
+    with open('/home/app/yt.json', 'r') as obj:
+        y_dict = json.load(obj)
+
+    # print(x_dict)
+    # print(y_dict)
+
+    x_train = []
+    y_train = []
+
+    for i in x_dict:
+        x_train.append([])
+        for j in x_dict[i]:
+            x_train[int(i)].append([x_dict[i][j]])
+
+    for i in y_dict:
+        y_train.append([y_dict[i]])
+
+    x_train = np.array(x_train)
+    y_train = np.array(y_train)
+    x_train = x_train.astype(np.float32)
+    y_train = y_train.astype(np.float32)
+
+    # 拉modle參數檔, x_train, y_train 下來
+    model = modelLoad(model_name='/home/app/model_setup.h5')
     model = train(model, x_train, y_train)
-    modelSave(model, 訓練完路徑)
-    訓練完路徑 = 儲存下來
-    return req
+    modelSave(model, model_name='/home/app/model_LSTM.h5')
+    
+    found = client.bucket_exists("model-lstm")
+    if not found:
+        client.make_bucket("model-lstm")
+    else:
+        print("Bucket 'model-lstm' already exists")
+
+    try:
+        client.fput_object('model-lstm', 'model-lstm.h5', '/home/app/model_LSTM.h5')
+    except S3Error as exc:
+        print("error occurred.", exc)
+
+    return 1
 
 def train(model, x_train, y_train):
     callback = EarlyStopping(monitor="loss", patience=10, verbose=1, mode="auto")
-    model.fit(x_train, y_train, epochs=3000, batch_size=128, callbacks=[callback])
+    model.fit(x_train, y_train, epochs=3, batch_size=128, callbacks=[callback])
     return model
 
 
-def modelSave(model, model_name='訓練完路徑'):
+def modelSave(model, model_name):
     model.save(model_name)
 
-def modelLoad(model_name='modle參數檔'):
+def modelLoad(model_name='/home/app/model_setup.h5'):
     model = tf.keras.models.load_model(model_name, compile = True)
     model.summary()
     return model
